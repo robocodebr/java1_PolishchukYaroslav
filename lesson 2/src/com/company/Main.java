f9e612fb35625a1880f68866c0b6c099ac43c9cc
robocodebr <robocodebr@gmail.com>
2020-09-13 15:57:06 +0300

    lesson2

---
+++ lesson 2/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        /*int day = 11;
+        int month = 6;
+        int year = 8;
+
+        if (day > month) {
+            System.out.println(day);
+        } else{ System.out.println(month)
+    }else if (year < month ){
+            { System.out.println(year);
+
+            String SurName  ="Polishchuk";
+            String SecondName  ="OLegovich";
+            String Name  ="Yaroslav";
+
+           if (SecondName.length()>Name.length() && SecondName.length()>SurName.length()){
+               System.out.println(SecondName);
+
+           } else if(Name.length()<SurName.length() &&Surname.length()>Name.length() )
+               System.out.println(SurName);
+           else
+           {
+               System.out.println(Name);
+           }
+
+         String first = "0123456";
+         String second = "0123456";
+         String third = "0123456";
+
+
+         Random r = new Random();
+         System.out.println(r.nextInt());
+
+        int r1 = (r.nextInt());
+        int r2 = (r.nextInt());
+        int r3 = (r.nextInt());
+
+        if (r1<r2 && r3<r2) {
+            System.out.println(r2);
+
+        } else if(r1>r2 && r1>r3) {
+            System.out.println(r1);
+        } else {
+            System.out.println(r3);*/
+        }
+
+
+
+
+    }
+
+
+
+//lesson2
+}
--- lesson1/src/com/company/Main.java
+++ lesson1/src/com/company/Main.java
@@ -3,35 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //User user = new User();
-        /*int i = 6;
-        byte varableName = 45;
-        char c;
-        c = 'd';
-        float f = 3.14f;
-        long l = 56;
-        double d = f;
-        /*int i2 = (int)l;
-        //boolean bol = true;
-        //boolean bol2 = false;
+        
+        int x = 14;
 
-        System.out.println(String.format("%.2f", d));
-        System.out.println(f);
-        System.out.println(l);
-        System.out.println(bol);
-        System.out.println(bol2);
-        */
-        int age = 12;
-        char Surname = 'P';
-        char Name = 'Y';
-        char SecondName = 'O';
-        boolean isMan = true;
+        System.out.println(x);
 
-        System.out.print(age+" ");
-        System.out.print(Surname+" "+Name+ " ");
-        System.out.print(SecondName+ " ");
-        System.out.println(isMan);
+        if(x = 17);
 
     }
 }
